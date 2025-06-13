a421095eee511908f8fc9911ff32275a99d2ebcf
saurav-mukherjee-telus <saurav.mukherjee@telusagcg.com>
2025-06-13 15:27:18 +1000

    Code cleanup

--- src/test/java/com/adaptris/webspheremq/test/DefaultFunctionalTest.java
+++ src/test/java/com/adaptris/webspheremq/test/DefaultFunctionalTest.java
@@ -16,8 +16,7 @@ import java.util.Hashtable;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DefaultFunctionalTest extends DockerComposeFunctionalTest {
-
-
+    
     private MQQueueManager queueManager;
 
     protected static String INTERLOK_SERVICE_NAME = "interlok-1";
@@ -25,9 +24,7 @@ public class DefaultFunctionalTest extends DockerComposeFunctionalTest {
     protected static String WEBSPHEREMQ_SERVICE_NAME = "webspheremq-1";
     protected static int WEBSPHEREMQ_PORT = 1414;
 
-    private static final String HOST = "webspheremq-1";
-
-    private final String QUEUE_NAME = "DEV.QUEUE.1";
+   private final String QUEUE_NAME = "DEV.QUEUE.1";
     private final String QUEUE_MANAGER_NAME = "QM1";
     private final String CHANNEL = "DEV.APP.SVRCONN";
 
@@ -40,15 +37,8 @@ public class DefaultFunctionalTest extends DockerComposeFunctionalTest {
     protected ComposeContainer setupContainers() throws Exception {
         return new ComposeContainer(new File("docker-compose.yml"))
                 .withExposedService(INTERLOK_SERVICE_NAME, INTERLOK_PORT, defaultWaitStrategy)
-                .withExposedService(WEBSPHEREMQ_SERVICE_NAME, WEBSPHEREMQ_PORT, defaultWaitStrategy);
+                .withExposedService(WEBSPHEREMQ_SERVICE_NAME, WEBSPHEREMQ_PORT);
     }
-
-    protected String getInterlokEndpoint(String path) {
-        InetSocketAddress address = getHostAddressForService(WEBSPHEREMQ_SERVICE_NAME, WEBSPHEREMQ_PORT);
-        if (!path.startsWith("/")) path = "/" + path;
-        return "tcp://" + address.getHostString() + ":" + address.getPort() + path;
-    }
-
 
     @Test
     public void test() throws Exception {
@@ -57,22 +47,12 @@ public class DefaultFunctionalTest extends DockerComposeFunctionalTest {
 
         Hashtable<String, Object> props = new Hashtable<>();
         props.put(MQConstants.TRANSPORT_PROPERTY, MQConstants.TRANSPORT_MQSERIES_CLIENT);
-        props.put(MQConstants.HOST_NAME_PROPERTY, mgmt.getHostName());
+        props.put(MQConstants.HOST_NAME_PROPERTY, mgmt.getHostString());
         props.put(MQConstants.PORT_PROPERTY, String.valueOf(mgmt.getPort()));
         props.put(MQConstants.CHANNEL_PROPERTY, CHANNEL);
 
         queueManager = new MQQueueManager(QUEUE_MANAGER_NAME, props);
 
         assertTrue(queueManager.isConnected(), "Should be connected to MQ");
-
-//        Properties mqProperties = new Properties();
-//        mqProperties.setProperty("mq.queueManager", QUEUE_NAME);
-//        mqProperties.setProperty("mq.host", "localhost");
-//        mqProperties.setProperty("mq.port", String.valueOf(WEBSPHEREMQ_PORT));
-//        mqProperties.setProperty("mq.channel", "TEST.CHANNEL");
-//        mqProperties.setProperty("mq.queue.input", "INPUT.QUEUE");
-//        mqProperties.setProperty("mq.queue.output", "OUTPUT.QUEUE");
-
-//        WebSphereMQService mqService = new WebSphereMQService(mqProperties);
     }
 }
